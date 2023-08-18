9d8a1be0db3f1edce7886bf40b91e816584b9dbe
gopelkujo <sigantengnoval@gmail.com>
2023-08-18 09:04:38 +0700

    :tada: Initialize next.js

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/index.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Gopel&apos;s Work</title>
+      </Head>
+      <div className={styles.container}>
+        <span className={styles.header}>
+          Hello world!<br />
+        </span>
+        <span className={styles.description}>
+          I&apos;m <a href="https://www.linkedin.com/in/gopel-kujo/" target="_blank">Gopel</a>, welcome to my portfolio website.
+        </span>
+      </div>
+      <span className={styles.comingSoon}>
+        coming soon🙈
+      </span>
+    </>
+  )
+}
