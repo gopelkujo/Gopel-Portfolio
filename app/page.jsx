390580f9dc175b123ab10d5f8af7c75aae8ce5fb
gopelkujo <sigantengnoval@gmail.com>
2023-11-23 21:21:10 +0700

    :lipstick: Modify home page UI

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,23 @@
-import Head from "next/head";
-import styles from "../styles/index.module.css";
+import Head from "next/head"
+import styles from "../styles/index.module.css"
+import gopelImage from "../src/assets/images/gopel-sleepy.png"
+import Image from "next/image"
 
 export default function Home() {
+  const description = "Hi👋🏻 My name is Nauval (a.k.a Gopel). I'am a profesional Web & Mobile Developer, but recently i do some desktop project. My passion is to translate the design into a real interface that user can use. I love design too, but i don't fit too much as a designer coz my design is based on my mood ehe✌🏻. Welcome to my portfolio! enjoy☕"
   return (
     <>
       <Head>
         <title>Gopel&apos;s Work</title>
       </Head>
       <div className={styles.container}>
-        <span className={styles.highlight}>
-          Hello world!<br />
-        </span>
+        <div className={styles.profileImage}/>
         <span className={styles.description}>
-          I&apos;m <a href="https://www.linkedin.com/in/gopel-kujo/" target="_blank">Gopel</a>, welcome to my portfolio website.
+          {description}
         </span>
       </div>
       <span className={styles.comingSoon}>
-        coming soon🙈
+        Built in Next JS and deploy on Vercel, with ❤ Take a tour to this project on <a href="https://github.com/gopelkujo/Gopel-Portfolio" target="_blank" rel="noopener noreferrer">Github</a>
       </span>
     </>
   )
