94894468ba9803fc6b20ab8171ab90d668ca33e8
Nauval <sigantengnoval@gmail.com>
2023-11-23 23:13:59 +0700

    🔖 Gopel Kujo Web Page Version 1.1 (#3)
    * :bulb: Add note to do next at page.jsx
    
    * :bento: Add gopel image in assets
    
    * :lipstick: Modify home page UI
    
    * :bento: Add menu item icon
    
    * :lipstick: Modify UI and content at home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,39 @@
-import Head from "next/head";
-import styles from "../styles/index.module.css";
+import Head from "next/head"
+import styles from "../styles/index.module.css"
+import gopelImage from "../src/assets/images/gopel-sleepy.png"
+import Image from "next/image"
 
 export default function Home() {
+  const description = "Hi👋🏻 My name is Nauval (a.k.a Gopel). I'am a profesional Web & Mobile Developer, but recently i do some desktop project too. My passion is to translate the design into a real interface that user can use the goods as comfortably as possible. I love design too, but i don't fit too much as a designer coz my design is based on my mood ehe✌🏻. I currently work as Mobile Developer for Javabook Indonesia onsite at East Jakarta, Indonesia. Welcome to my page! enjoy☕"
   return (
-    <>
-      <Head>
-        <title>Gopel&apos;s Work</title>
-      </Head>
-      <div className={styles.container}>
-        <span className={styles.highlight}>
-          Hello world!<br />
-        </span>
-        <span className={styles.description}>
-          I&apos;m <a href="https://www.linkedin.com/in/gopel-kujo/" target="_blank">Gopel</a>, welcome to my portfolio website.
-        </span>
-      </div>
-      <span className={styles.comingSoon}>
-        coming soon🙈
+    <div className={styles.container}>
+      <div className={styles.profileImage} />
+      <span className={styles.description}>
+        {description}
       </span>
-    </>
+    </div>
   )
 }
+
+/* 
+ * Page Routing Note
+ *
+ * Page route based on folder in top-level folder "app".
+ * Structure: 
+ *  /app:
+ *      /contact (folder)
+ *        /page.jsx (file as contact code)
+ * Example route: localhost:3000/contact
+ * 
+*/
+
+/*
+ * Assets Note
+ * Add folder "src" in top-level folder to keep assets files
+ * Example:
+ *  /src:
+ *    /images
+ *      /image.png
+ *    /styles
+ *      /globals.css
+*/
