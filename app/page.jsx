7c46e3daa46742e31017e8db241e5466ede8db35
gopelkujo <sigantengnoval@gmail.com>
2024-07-30 18:59:24 +0700

    :lipstick: Modify index content

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,16 @@ import Image from "next/image"
 
 export default function Home() {
   const description = "Hi👋🏻 My name is Nauval (a.k.a Gopel). I'am a profesional Web & Mobile Developer, but recently i do some desktop project too. My passion is to translate the design into a real interface that user can use the goods as comfortably as possible. I love design too, but i don't fit too much as a designer coz my design is based on my mood ehe✌🏻. I currently work as Mobile Developer for Javabook Indonesia onsite at East Jakarta, Indonesia. Welcome to my page! enjoy☕"
+  const greet = "Hello, nice to meet you. I am Gopel!";
   return (
     <div className={styles.container}>
       <div className={styles.profileImage} />
-      <span className={styles.description}>
-        {description}
+      <span className={styles.greetText}>
+        {greet}
       </span>
+      {/* <span className={styles.description}>
+        {description}
+      </span> */}
     </div>
   )
 }
