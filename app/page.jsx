2525a54ee6533cfc8264acc3fb9beae5a3b5f0b0
Nauval <sigantengnoval@gmail.com>
2024-12-31 17:26:53 +0700

    💄 Home page layout UI modification (#32)
    * :art: Improve long string format
    
    * :construction: On progress selected works
    
    * :art: Improve variable naming
    
    * :lipstick: Construct project layout
    
    * :hammer: Update periplus apps works
    
    * :hammer: Update website works
    
    * :seedling: Add about page seeds
    
    * :fire: Remove unused code
    
    * :hammer: Add other works section
    
    * :hammer: Add selected work title
    
    * :bug: Fix double css declaration
    
    * :hammer: Add other works
    
    * :hammer: Add contribution section
    
    * :hammer: Adjust css variable name
    
    * :bug: Fix missing playstore and appstore badge
    
    * :iphone: Responsive home page
    
    * :hammer: Hide about page with underconstruction announcement

--- app/page.jsx
+++ app/page.jsx
@@ -1,113 +1,417 @@
 import styles from "../styles/index.module.css";
 import Link from "next/link";
+import Image from "next/image";
+
+// Import PHC images
+import PhcIcon from "../src/assets/images/phc/icon.jpg";
+import PhcLogin from "../src/assets/images/phc/login.png";
+import PhcHome from "../src/assets/images/phc/home.png";
+import PhcAttendance from "../src/assets/images/phc/attendance.png";
+import PhcAttendanceForm from "../src/assets/images/phc/attendance_form.png";
+import PhcNotification from "../src/assets/images/phc/notification.png";
+import PhcProfile from "../src/assets/images/phc/profile.png";
+import AppStoreBadge from "../src/assets/images/app_store_badge.svg";
+import GooglePlayBadge from "../src/assets/images/google_play_badge.png";
+
+// Import Periplus Apps images
+import PPlusIcon from "../src/assets/images/periplus_apps/icon.jpeg";
+import PPlusHome from "../src/assets/images/periplus_apps/home.png";
+import PPlusCategory from "../src/assets/images/periplus_apps/category.png";
+import PPlusLogin from "../src/assets/images/periplus_apps/login.png";
+import PPlusProfile from "../src/assets/images/periplus_apps/profile.png";
+import PPlusBook from "../src/assets/images/periplus_apps/book.png";
+import PPlusCart from "../src/assets/images/periplus_apps/cart.png";
+import PPlusPayment from "../src/assets/images/periplus_apps/payment.png";
+import PPlusTransaction from "../src/assets/images/periplus_apps/transaction.png";
+
+// Import Multi Piranti Jaya images
+import MpjIcon from "../src/assets/images/mpj2/icon.png";
+import MpjHome1 from "../src/assets/images/mpj2/home_1.png";
+import MpjHome2 from "../src/assets/images/mpj2/home_2.png";
+import MpjHome3 from "../src/assets/images/mpj2/home_3.png";
+import MpjHome4 from "../src/assets/images/mpj2/home_4.png";
+import MpjHome5 from "../src/assets/images/mpj2/home_5.png";
+import MpjAbout from "../src/assets/images/mpj2/about.png";
+import MpjActivity from "../src/assets/images/mpj2/activity.png";
+import MpjService from "../src/assets/images/mpj2/service.png";
+import MpjContactUs from "../src/assets/images/mpj2/contact_us.png";
+
+// Import SINBADA images
+import Sinbada from "../src/assets/images/sinbada/1.png";
+
+// Import SIINVENT images
+import Siinvent from "../src/assets/images/siinvent/1.png";
+
+// Import GORAS images
+import Goras from "../src/assets/images/goras/goras_main.png";
+
+// Import Github images
+import GithubIcon from "../src/assets/images/github.svg";
+
+// Import Stackoverflow images
+import StackoverflowIcon from "../src/assets/images/stackoverflow.png";
 
 export default function Home() {
-  const greet = "Hello, nice to meet you. I am Gopel!";
-  const takeATour =
-    "Hey! I'm a front-end web and mobile developer with a passion for turning beautiful designs into user-friendly apps. I really enjoy coding because, let's be real, it just looks cool to me, haha. When I'm not busy with code, you'll probably find me out exploring nature on my motorcycle ─ it's the perfect escape!";
-  return (
-    <div className={styles.pageContainer}>
+  const greetingText = "Hello, nice to meet you. I am Gopel!";
+
+  const introductionText =
+    "Hey! I'm a front-end web and mobile developer with a passion for turning \
+    beautiful designs into user-friendly apps. I really enjoy coding because, \
+    let's be real, it just looks cool to me, haha. When I'm not busy with \
+    code, you'll probably find me out exploring nature on my motorcycle ─ it's \
+    the perfect escape!";
+
+  const phcImages = [
+    { img: PhcLogin, alt: "Login Page" },
+    { img: PhcHome, alt: "Home Page" },
+    { img: PhcAttendance, alt: "Attendance Page" },
+    { img: PhcAttendanceForm, alt: "Attendance Form Page" },
+    { img: PhcNotification, alt: "Notification Page" },
+    { img: PhcProfile, alt: "Profile Page" },
+  ];
+
+  const pplusImages = [
+    { img: PPlusHome, alt: "Home" },
+    { img: PPlusCategory, alt: "Category" },
+    { img: PPlusLogin, alt: "Login" },
+    { img: PPlusProfile, alt: "Profile" },
+    { img: PPlusBook, alt: "Book" },
+    { img: PPlusCart, alt: "Cart" },
+    { img: PPlusPayment, alt: "Payment" },
+    { img: PPlusTransaction, alt: "Transaction" },
+  ];
+
+  const mpjImages = [
+    { img: MpjHome1, alt: "Home 1" },
+    { img: MpjHome2, alt: "Home 2" },
+    { img: MpjHome3, alt: "Home 3" },
+    { img: MpjHome4, alt: "Home 4" },
+    { img: MpjHome5, alt: "Home 5" },
+    { img: MpjAbout, alt: "About" },
+    { img: MpjActivity, alt: "Activity" },
+    { img: MpjService, alt: "Service" },
+    { img: MpjContactUs, alt: "Contact Us" },
+  ];
+
+  function BuildHeader() {
+    return (
       <div className={styles.greetContainer}>
         <div className={styles.greetAndTourTextContainer}>
-          <span className={styles.greetText}>{greet}</span>
-          <span className={styles.takeATourText}>{takeATour}</span>
+          <span className={styles.greetText}>{greetingText}</span>
+          <span className={styles.takeATourText}>{introductionText}</span>
         </div>
         <div className={styles.profileImage} />
       </div>
-      <div className={styles.experienceBox}>
-        <span className={styles.subTitle}>Education</span>
-        <ul>
-          <li>
-            <div className={styles.experienceTitle}>
-              <h1>Polytechnic State of Bandung </h1>
-              <h2>(2019 - 2023)</h2>
-            </div>
-            <i>
-              Teknik Jurusan Komputer dan Informatika - D4 Teknik Informatika
-            </i>
-            <br />
-            With the final project title “Development of Web-Based Book Sales
-            Prediction Application on E-Commerce Markets at PT Periplus
-            Bookindo”
-            <div className={styles.tools}>
-              <ul>
-                <li>React</li>
-                <li>Python</li>
-                <li>Mysql</li>
-                <li>Machine Learning</li>
-                <li>Multiple Linear Regression</li>
-              </ul>
-            </div>
-          </li>
-        </ul>
+    );
+  }
+
+  function BuildSelectedWork() {
+    const selectedWorks = [
+      {
+        icon: PPlusIcon,
+        title: "Periplus",
+        description: "Periplus online bookstore, 10k+ total downloads.",
+        appstoreLink: "https://apps.apple.com/id/app/periplus/id6444208499",
+        playstoreLink:
+          "https://play.google.com/store/apps/details?id=com.bookindo.periplus.periplus&hl=en",
+        techs: ["Figma, Flutter, Firebase, Midtrans"],
+        images: pplusImages,
+      },
+      {
+        icon: PhcIcon,
+        title: "Periplus Human Capital",
+        description:
+          "An application for employee management, mainly for employee attendance. Published privately on App Store and Play Store.",
+        techs: ["Figma, Flutter, Firebase, Geolocation"],
+        images: phcImages,
+      },
+      {
+        icon: MpjIcon,
+        title: "Multi Piranti Jaya",
+        description: "Profile company website of Multi Piranti Jaya.",
+        websiteLink: "https://multipirantijaya.com/",
+        techs: ["React JS, MUI, Wordpress, Javascript"],
+        images: mpjImages,
+        isImageWide: true,
+      },
+    ];
+
+    return (
+      <div className={styles.pageContainer}>
+        <div className={styles.selectedWork}>Some of My Selected Works</div>
+
+        {selectedWorks.map((item, index) => {
+          return (
+            <BuildWork
+              key={index}
+              icon={item.icon}
+              title={item.title}
+              appstoreLink={item.appstoreLink}
+              playstoreLink={item.playstoreLink}
+              description={item.description}
+              websiteLink={item.websiteLink}
+              techs={item.techs}
+              images={item.images}
+              isImageWide={item.isImageWide ?? false}
+            />
+          );
+        })}
       </div>
-      <div className={styles.experienceBox}>
-        <span className={styles.subTitle}>Work Experience</span>
-        <ul>
-          <li>
-            <div className={styles.experienceTitle}>
-              <h1>Javabook Indonesia</h1>
-              <h2>(July 2022 - now)</h2>
+    );
+  }
+
+  function BuildWork({
+    icon,
+    title,
+    description,
+    appstoreLink,
+    playstoreLink,
+    websiteLink,
+    techs,
+    images,
+    isImageWide = false,
+  }) {
+    return (
+      <div className={styles.selectedWorkBox}>
+        <div className={styles.selectedWorkBoxtitle}>
+          <Image src={icon} alt="logo" className={styles.selectedWorkBoxIcon} />
+
+          <div className={styles.selectedWorkBoxtitleDesc}>
+            <h1>{title}</h1>
+            <h2>{description}</h2>
+          </div>
+
+          {appstoreLink == null &&
+          playstoreLink == null &&
+          websiteLink == null ? null : (
+            <div className={styles.selectedWorkBoxStore}>
+              {appstoreLink ? (
+                <Link
+                  href={appstoreLink}
+                  target="_blank"
+                  style={{ marginRight: "0.5rem" }}
+                >
+                  <Image
+                    src={AppStoreBadge}
+                    alt="Google Play Store Icon"
+                    width="auto"
+                    height="auto"
+                  />
+                </Link>
+              ) : null}
+              {playstoreLink != null ? (
+                <Link href={playstoreLink} target="_blank">
+                  <Image
+                    src={GooglePlayBadge}
+                    alt="Google Play Store Icon"
+                    width={115}
+                    height={100}
+                    loading="lazy"
+                  />
+                </Link>
+              ) : null}
+              {websiteLink != null ? (
+                <Link
+                  href={websiteLink}
+                  target="_blank"
+                  className={styles.visitWebsiteBox}
+                >
+                  Visit Website
+                </Link>
+              ) : null}
             </div>
-            <i>Mobile Developer</i>
-            <br />
-            Working on develop and maintaining Periplus Apps (Book Store Mobile
-            Application) and Periplus Human Capital (Human Resource Mobile
-            Application) based on Android and iOS using Flutter.
-            <div className={styles.tools}>
-              <ul>
-                <li>Flutter</li>
-                <li>iOS</li>
-                <li>Android</li>
-                <li>Android Studio</li>
-                <li>Xcode</li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.experienceBox}>
-        <span className={styles.subTitle}>Project</span>
-        <br />
-        <div className={styles.projectContainer}>
-          <Link href="/multi-piranti-jaya">
-            <div className={styles.projectBox}>
-              <h1>Multi Piranti Jaya</h1>
-              <p>Company profile website based on ReactJS and Wordpress CMS.</p>
-              <div className={styles.tools}>
-                <ul>
-                  <li>ReactJS</li>
-                  <li>MUI</li>
-                </ul>
-              </div>
-            </div>
-          </Link>
-          <Link href="/sinbada">
-            <div className={styles.projectBox}>
-              <h1>SINBADA</h1>
-              <p>Inventory system website based on ReactJS and Ruby.</p>
-              <div className={styles.tools}>
-                <ul>
-                  <li>ReactJS</li>
-                  <li>MUI</li>
-                </ul>
-              </div>
-            </div>
-          </Link>
-          <Link href="/siinvent">
-            <div className={styles.projectBox}>
-              <h1>SIINVENT</h1>
-              <p>Inventory system website based on ReactJS and ExpressJS.</p>
-              <div className={styles.tools}>
-                <ul>
-                  <li>ReactJS</li>
-                  <li>MUI</li>
-                </ul>
-              </div>
-            </div>
-          </Link>
+          )}
+        </div>
+
+        <div className={styles.selectedWorkBoxStoreMobile}>
+          {appstoreLink ? (
+            <Link
+              href={appstoreLink}
+              target="_blank"
+              style={{ marginRight: "0.5rem" }}
+            >
+              <Image
+                src={AppStoreBadge}
+                alt="Google Play Store Icon"
+                width="auto"
+                height="auto"
+              />
+            </Link>
+          ) : null}
+          {playstoreLink != null ? (
+            <Link href={playstoreLink} target="_blank">
+              <Image
+                src={GooglePlayBadge}
+                alt="Google Play Store Icon"
+                width={115}
+                height={100}
+                loading="lazy"
+              />
+            </Link>
+          ) : null}
+          {websiteLink != null ? (
+            <Link
+              href={websiteLink}
+              target="_blank"
+              className={styles.visitWebsiteBox}
+            >
+              Visit Website
+            </Link>
+          ) : null}
+        </div>
+
+        <div className={styles.selectedWorkBoxTech}>
+          <ul>
+            {techs.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
+        </div>
+
+        <div className={styles.selectedWorkBoxImages}>
+          {images.map((item, index) => {
+            return (
+              <Image
+                key={index}
+                src={item.img}
+                alt={item.alt}
+                className={
+                  isImageWide
+                    ? styles.selectedWorkBoxWideImg
+                    : styles.selectedWorkBoxLongImg
+                }
+              />
+            );
+          })}
         </div>
       </div>
+    );
+  }
+
+  function BuildOtherWorks() {
+    function BuildItem({ img, url, title, desc, stacks }) {
+      return (
+        <Link href={url} target="_blank">
+          <div className={styles.projectBox}>
+            <Image src={img.url} alt={img.alt} loading="lazy" />
+            <h1>{title}</h1>
+            <p>{desc}</p>
+            <ul>
+              {stacks.map((item, index) => {
+                return <li key={index}>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </Link>
+      );
+    }
+
+    return (
+      <div className={styles.otherProjectBox}>
+        <span className={styles.subTitle}>Other Project</span>
+        <br />
+        <div className={styles.projectContainer}>
+          <BuildItem
+            img={{ url: Sinbada, alt: "test" }}
+            url="https://github.com/SekelompokOrangKuat/PROJECTCUAN/tree/frontend"
+            title="SINBADA"
+            desc="Inventory system website based on ReactJS and Ruby."
+            stacks={["ReactJS", "MUI", "Javascript"]}
+          />
+          <BuildItem
+            img={{ url: Siinvent, alt: "test" }}
+            url="https://github.com/SekelompokOrangKuat/ProjectInventaris/tree/dev"
+            title="SIINVENT"
+            desc="Inventory system website based on ReactJS and ExpressJS."
+            stacks={["ReactJS", "MUI", "Javascript"]}
+          />
+          <BuildItem
+            img={{ url: Goras, alt: "test" }}
+            url="https://github.com/gopelkujo/gorasproject"
+            title="Goras Ludo Games"
+            desc="My team project at first year in collage. Goras Ludo Games is a board game that is similar to playing snakes and ladders."
+            stacks={["C"]}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  function BuildContributions() {
+    const contributionDataList = [
+      {
+        title: "Stackoverflow",
+        desc: "Stack Overflow is best known for its public Q&A platform, which attracts millions of visitors every month who come to ask questions, learn, and share technical knowledge.",
+        link: "https://stackoverflow.com/users/12566347/gopelkujo",
+        icon: StackoverflowIcon,
+        data: [
+          {
+            title: "More than 700++ reputation",
+            desc: "Actively answers for questions on stackoverflow. And due to over 500 reputaion, i have side quest that review other use question/answer for give some feedback, such as what question/answer that can be improved or if the question/answer not easly can be understood by other user i can give a feedback that how the question/answer can be more easly understood by other.",
+          },
+          {
+            title: "50+ Answer and question",
+            desc: "Most answer and question are in flutter section, but does not close for other parts of discussion.",
+          },
+        ],
+      },
+      {
+        title: "Github",
+        desc: "GitHub is a proprietary developer platform that allows developers to create, store, manage, and share their code. It uses Git to provide distributed version control. It is commonly used to host open source software development projects.",
+        link: "https://github.com/gopelkujo",
+        icon: GithubIcon,
+        data: [
+          {
+            title: "Open issue on open source project",
+            desc: "Most project that i had openned the issue are open source project that i used in my project. If the project that i had opened an issue doesn't mantained anymore, and my project needed so much, then i fork the repo and fix it by myself for my project.",
+          },
+          {
+            title: "Contribute on open source project",
+            desc: "I contributed to some open source project that related with my expertise as a frontend developer such as npm package (npmjs.com) and flutter package (pub.dev).",
+          },
+        ],
+      },
+    ];
+
+    return (
+      <div className={styles.contributionsWork}>
+        <span className={styles.subTitle}>Contribution</span>
+
+        {contributionDataList.map((item, itemIndex) => {
+          return (
+            <div key={itemIndex} className={styles.contribution}>
+              <div className={styles.contributionHeader}>
+                <Image
+                  src={item.icon}
+                  alt={item.title + " logo"}
+                  className={styles.selectedWorkBoxIcon}
+                />
+                <div className={styles.contributionTitle}>
+                  <Link href={item.link} target="_blank">
+                    {item.title}
+                  </Link>
+                  <h2>{item.desc}</h2>
+                </div>
+              </div>
+              <ul>
+                {item.data.map((data, dataIndex) => {
+                  return (
+                    <li key={dataIndex}>
+                      <a href="#">{data.title}</a>
+                      <p>{data.desc}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  function BuildQuote() {
+    return (
       <div className={styles.quoteBox}>
         <p>
           &quot;When you arise in the morning, think of what a precious
@@ -116,6 +420,16 @@ export default function Home() {
         </p>
         <footer>— Aurelius, Marcus</footer>
       </div>
+    );
+  }
+
+  return (
+    <div className={styles.pageContainer}>
+      <BuildHeader />
+      <BuildSelectedWork />
+      <BuildOtherWorks />
+      <BuildContributions />
+      <BuildQuote />
     </div>
   );
 }
